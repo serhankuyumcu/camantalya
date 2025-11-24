7fe9d34264eaaf95ba267e1b65cee06f3c0044d2
Serhan Kuyumcu <serhankuyumcu7@gmail.com>
2025-11-24 13:17:30 +0300

    vercel add

--- cam-antalya/astro.config.mjs
+++ cam-antalya/astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind'; 
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless'; // Vercel adaptörünü içe akta 
 
 // https://astro.build/config
 export default defineConfig({
   // React ve Tailwind entegrasyonları
   integrations: [
     react(),
-    tailwind()
+    tailwind(),
   ],
+  output: 'server',
+   adapter: vercel({
+    // serverless veya edge fonksiyonlarını burada ayarlayabilirsiniz
+    // buildOutput: true, // Varsayılan olarak Astro, Vercel'de çıktıyı ayarlar.
+  }),
 });
